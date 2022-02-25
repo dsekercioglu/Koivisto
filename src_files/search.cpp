6ef679eeecfd34cfebb24e27a1c71b20b752b2b5
dsekercioglu <dsekercioglu2003@gmail.com>
2022-02-25 07:36:42 +0300

    Less aggressive pruning

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -952,7 +952,7 @@ Score Search::qSearch(Board* b, Score alpha, Score beta, Depth ply, ThreadData* 
             > beta)
             return beta;
 
-        if (isCapture(m) && sd->getCaptureHistory(m, b->getActivePlayer()) < -92) {
+        if (isCapture(m) && sd->getCaptureHistory(m, b->getActivePlayer()) < -128) {
             continue;
         }   
         // *******************************************************************************************
