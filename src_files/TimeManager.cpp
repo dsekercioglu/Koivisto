36412f9b61412f2cb826ffc3389eb265c11b0771
doruk <dsekercioglu2003@gmail.com>
2022-02-28 15:29:23 +0300

    Amplify in opposite direction

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -163,8 +163,7 @@ bool TimeManager::rootTimeLeft(int score) {
         return false;
 
     // Amplify the scores
-    int revScore = 100 - score;
-    score = 100 - (revScore * revScore / 100);
+    score = score * score / 100;
     // if we are above the maximum allowed time at root, stop
     if (elapsed >= timeToUse*50/std::max(score, 30))
         return false;
