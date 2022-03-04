5b2ea4bc6ce5ce9ebd0d3d7ba1cc4294bf463035
doruk <dsekercioglu2003@gmail.com>
2022-03-04 19:29:21 +0300

    Ensure follow up move history is lower than 0 as well

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -686,7 +686,8 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
 
                 int prune_margin = -128 * depth;
                 int cm_hist = sd->getCounterMoveHistory(m, b->getActivePlayer(), b->getPreviousMove());
-                if (!inCheck && staticEval <= alpha && depth <= 3 && cm_hist < prune_margin) {
+                int fm_hist = sd->getFollowupMoveHistory(m, b->getActivePlayer(), b->getPreviousMove(2));
+                if (!inCheck && staticEval <= alpha && depth <= 3 && cm_hist < prune_margin && fm_hist < 0) {
                     continue;
                 }
             }
