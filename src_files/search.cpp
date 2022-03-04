f024e88a2e79d4bbc7528d75d01b32a86b10d551
doruk <dsekercioglu2003@gmail.com>
2022-03-04 13:10:37 +0300

    Linear formula for pruning at up to depth 2

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -684,8 +684,8 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                     continue;
                 }
 
-                int cmh_margin = -128 * depth;
-                if (!inCheck && depth <= 3
+                int cmh_margin = -170 * depth;
+                if (!inCheck && depth < 3
                     && sd->getCounterMoveHistory(m, b->getActivePlayer(), b->getPreviousMove(2))
                            < cmh_margin) {
                     continue;
