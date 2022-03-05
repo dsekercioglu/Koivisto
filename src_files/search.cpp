84b6641fc7300656b51289e1d6b7445347f89363
doruk <dsekercioglu2003@gmail.com>
2022-03-05 16:06:37 +0300

    Linear margin for double history prune

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -677,7 +677,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                 // if the history score for a move is really bad at low depth, dont consider this
                 // move.
                 // ***********************************************************************************
-                int h_margin = std::min(70 - 15 * (depth * (depth + isImproving)), 0);
+                int h_margin = -85 * (depth + isImproving);
 
                 int cm_hist =
                     sd->getCounterMoveHistory(m, b->getActivePlayer(), b->getPreviousMove());
