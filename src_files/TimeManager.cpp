f401316f1bfa581b9ea19da1fd968cae66a3936f
doruk <dsekercioglu2003@gmail.com>
2022-03-05 10:40:09 +0300

    40k iteration tuning values

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -107,9 +107,9 @@ bool TimeManager::rootTimeLeft(int nodeScore, int evalScore) const {
     if (force_stop)
         return false;
 
-    nodeScore = 110 - std::min(nodeScore, 90);
+    nodeScore = 105 - std::min(nodeScore, 82);
     
-    evalScore = std::min(std::max(50, 50 + evalScore), 80);
+    evalScore = std::min(std::max(52, 52 + evalScore), 83);
 
     int elapsed = elapsedTime();
     
@@ -124,7 +124,7 @@ bool TimeManager::rootTimeLeft(int nodeScore, int evalScore) const {
     // 100, we half the time to use. If it's lower than 30, it reaches a maximum of 1.4 times the
     // original time to use.
     if(    match_time_limit.enabled
-        && match_time_limit.time_to_use * nodeScore / 100 * evalScore / 65 < elapsed)
+        && match_time_limit.time_to_use * nodeScore / 100 * evalScore / 63 < elapsed)
         return false;
     
     return true;
