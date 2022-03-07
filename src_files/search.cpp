315607b9d2710baff6a29a7938ac90f8d97f17ce
doruk <dsekercioglu2003@gmail.com>
2022-03-07 22:50:41 +0300

    Only razor if less than 2 threats

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -513,7 +513,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // razoring:
         // if a qsearch on the current position is far below beta at low depth, we can fail soft.
         // **********************************************************************************************************
-        if (depth <= 3 && staticEval + RAZOR_MARGIN < beta) {
+        if (depth <= 3 && staticEval + RAZOR_MARGIN < beta && enemyThreats < 2) {
             score = qSearch(b, alpha, beta, ply, td);
             if (score < beta) {
                 return score;
