6de552d5ab0d78bc262aa93cd3959278bcfdc392
doruk <dsekercioglu2003@gmail.com>
2022-03-09 16:25:51 +0300

    More LMR if the enemy doesn't have any threats and we do

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -788,8 +788,8 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             lmr         = lmr - history / 150;
             lmr += !isImproving;
             lmr -= pv;
-            if (ownThreats)
-                lmr += !enemyThreats;
+            if (ownThreats > 1)
+                lmr += 1 + !enemyThreats;
             if (!sd->targetReached) 
                 lmr++;
             if (sd->isKiller(m, ply, b->getActivePlayer()))
